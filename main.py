0a0f0fc50738eee6545f656f8000df82b2156092
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2017-11-16 10:56:00 +0000

    Outline initial structure

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+baseURL = 'https://www.songkick.com/metro_areas/24495-uk-leeds'
+queryURL = 'https://www.songkick.com/metro_areas/24495-uk-leeds?page={0}'
+
+## Get base page
+
+
+## Get number of upcoming concerts
+
+
+## Work out the number of pages
+
+
+## For each page, get source
+
+
+## For each source page, identify individual events
+
+
+## For each event get event metadata
+
