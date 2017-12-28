d20a25bd7a0eb516f136bca2cf0e6ff891915aef
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2017-12-28 17:30:08 +0100

    fiddling with GoogleSheets

--- GoogleSheetsWorker.py
+++ GoogleSheetsWorker.py
@@ -1,41 +1,36 @@
 # Imports
 from Worker import Worker
-#import gspread
-#from oauth2client.service_account import ServiceAccountCredentials
+# import gspread
+# from oauth2client.service_account import ServiceAccountCredentials
 
-## Creating client
-#scope = ['https://spreadsheets.google.com/feeds']
-#creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-#client = gspread.authorize(creds)
+# # Set up sheet interaction
+# sheet = client.open('sample_gigs').sheet1
 
-## Set up sheet interaction
-#sheet = client.open('sample_gigs').sheet1
+# # Get all records
+# list_of_hashes = sheet.get_all_records()
+# print(list_of_hashes)
 
-## Get all records
-#list_of_hashes = sheet.get_all_records()
-#print(list_of_hashes)
+# # Records as list of lists
+# lOL = sheet.get_all_values()
 
-## Records as list of lists
-#lOL = sheet.get_all_values()
+# # Records from individual row, column or cell
+# row = sheet.row_values(1)
+# column = sheet.col_values(1)
+# sheet.cell(1,1).value()
 
-## Records from individual row, column or cell
-#row = sheet.row_values(1)
-#column = sheet.col_values(1)
-#sheet.cell(1,1).value()
+# # Writing to sheet
+# sheet.update_cell(1, 1, "Inserting this text here")
 
-## Writing to sheet
-#sheet.update_cell(1, 1, "Inserting this text here")
+# # Writing a row
+# row = ['This', 'Is', 'A', 'Row']
+# index = 1
+# sheet.insert_row(row, index)
 
-## Writing a row
-#row = ['This', 'Is', 'A', 'Row']
-#index = 1
-#sheet.insert_row(row, index)
+# # Deleting a row
+# sheet.delete_row(1)
 
-## Deleting a row
-#sheet.delete_row(1)
-
-## Getting number of rows
-#numberOfRows = sheet.row_count()
+# # Getting number of rows
+# numberOfRows = sheet.row_count()
 
 # Class
 class GoogleSheetsWorker(Worker):
@@ -49,6 +44,11 @@ class GoogleSheetsWorker(Worker):
         self.stageDict = {'readData': self.readData,
                             'processData': self.readData,
                             'writeData': self.writeData}
+
+        # Creating client
+        scope = ['https://spreadsheets.google.com/feeds']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        client = gspread.authorize(creds)
 
     def readData(self):
         pass
