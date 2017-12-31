6593b87f7a5fb4a20986165bd2428ea502504989
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2017-12-31 17:09:00 +0100

    packaging Workers

---
+++ Workers/__init__.py
@@ -0,0 +1,5 @@
+from Worker import Worker
+from WebWorker import WebWorker
+from SongKickWorker import SongKickWorker
+from FileWriteWorker import FileWriteWorker
+from DataAnalysisWorker import DataAnalysisWorker
