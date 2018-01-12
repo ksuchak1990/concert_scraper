6ea7743460158f97212fb02edf9149ed95988a3d
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2018-01-12 17:12:38 +0000

    fiddling

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 # Imports
 import sys
 sys.path.append('./Workers')
-from Workers import SongKickWorker, FileWriteWorker, DataAnalysisWorker
+from Workers import SongKickWorker, FileWriteWorker, DataAnalysisWorker, WatchWorker
 
 # Running
-s = SongKickWorker()
-s.work(start='makeCatalogue', end='supplementMetadata')
+# s = SongKickWorker()
+# s.work(start='makeCatalogue', end='supplementMetadata')
 
-fW = FileWriteWorker()
-fW.work(start='importData', end='writeData')
+# fW = FileWriteWorker()
+# fW.work(start='importData', end='writeData')
 
-dAW = DataAnalysisWorker()
-dAW.work(start='importData', end='makeFigures')
+# dAW = DataAnalysisWorker()
+# dAW.work(start='importData', end='makeFigures')
+
+wW = WatchWorker()
+wW.work(start='importData', end='getEventsByGenre')
