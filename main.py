bdbe698def9c54ccc2b4d5568fb09b8cbee84689
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2018-03-28 10:29:10 +0100

    getting things running

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 # Imports
 import sys
 sys.path.append('./Workers')
-from Workers import SongKickWorker, FileWriteWorker, DataAnalysisWorker, WatchWorker
+from Workers import SongKickWorker, FileWriteWorker, DataAnalysisWorker
 
 # Running
-# s = SongKickWorker()
-# s.work(start='makeCatalogue', end='supplementMetadata')
+s = SongKickWorker()
+s.work(start='makeCatalogue', end='supplementMetadata')
 
-# fW = FileWriteWorker()
-# fW.work(start='importData', end='writeData')
+fW = FileWriteWorker()
+fW.work(start='importData', end='writeData')
 
-# dAW = DataAnalysisWorker()
-# dAW.work(start='importData', end='makeFigures')
+dAW = DataAnalysisWorker()
+dAW.work(start='importData', end='makeFigures')
 
-wW = WatchWorker()
-wW.work(start='importData', end='getEventsByGenre')
+# wW = WatchWorker()
+# wW.work(start='importData', end='checkWatchlists')
+
+# sW = ScopusWorker()
+# sW.work(start='getRobsStats', end='getRobsStats')
+
+# import requests
+# import json
+
+# resp = requests.get("http://api.elsevier.com/content/author?author_id=7004212771&view=metrics",
+#                     headers={'Accept':'application/json',
+#                              'X-ELS-APIKey': '7e059441b3fcd16c752c50b628747a01'})
+
+# print(json.dumps(resp.json(),
+#                  sort_keys=True,
+#                  indent=4, separators=(',', ': ')))
