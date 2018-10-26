323d4a8bbf58a569a0bf484222a322d352bdbd5f
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2018-10-26 23:47:07 +0100

    working on filmfest stuff

---
+++ Workers/LIFFWorker.py
@@ -0,0 +1,50 @@
+from WebWorker import WebWorker
+
+class LIFFWorker(WebWorker):
+
+    def __init__(self):
+        super().__init__()
+
+        self.product = 'films'
+
+        # Stages
+        self.stageList = ['downloadHome', 'parseHome', 'makeEventURLs',
+                            'downloadEvents', 'parseEvents']
+
+        self.stageDict = {'downloadHome': self.downloadHome,
+                        'parseHome': self.parseHome,
+                        'makeEventURLs': self.makeEventURLs,
+                        'downloadEvents': self.downloadEvents,
+                        'parseEvents': self.parseEvents}
+        
+        # Info
+        self.homeURL = 'https://www.leedsfilmcity.com/film-festivals/leeds-international-film-festival/liff-2018-programme/?Date=All+Dates&Strand=&Country=&Venue=&SortOrder=0&PageSize=10000&Page=1#festival-filter-form'
+        self.queryURL = 'https://www.leedsfilmcity.com{0}'
+
+    # stage functions
+    def downloadHome(self):
+        return self.requestURL(self.homeURL)
+
+    def parseHome(self, pageCode):
+        codeSection = self.restrict(inputString=pageCode, startStr='<section id="film-listing" class="event-listing">', endStr='</section>')
+        eventSections = codeSection.split('<div class="span4 event-item">')[1:]
+        eventList = [self.restrict(inputString=x, startStr='a href=\"', endStr='\">') for x in eventSections]
+        return list(set(eventList))
+    
+    def makeEventURLs(self, events):
+        endings = set(events)
+        return [self.queryURL.format(event) for event in endings]
+    
+    def downloadEvents(self, urls):
+        return [self.requestURL(inputString=url) for url in urls]
+    
+    def parseEvents(self, events):
+        return [self.parseEvent(event) for event in events]
+    
+    def parseEvent(self, event):
+        pass
+    
+    def makeMetadata(self, source):
+        md = dict()
+
+        return md
--- Workers/__init__.py
+++ Workers/__init__.py
@@ -3,5 +3,6 @@ from WebWorker import WebWorker
 from SongKickWorker import SongKickWorker
 from FileWriteWorker import FileWriteWorker
 from DataAnalysisWorker import DataAnalysisWorker
+from LIFFWorker import LIFFWorker
 # from WatchWorker import WatchWorker
 # from ScopusWorker import ScopusWorker
